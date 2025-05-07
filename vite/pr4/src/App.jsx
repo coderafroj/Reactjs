0a71b4414a3f50297cdcacf4337d8579c05eae1c
coderafroj <coderafroj@gmail.com>
2025-05-07 17:36:14 +0530

    add

--- vite/pr4/src/App.jsx
+++ vite/pr4/src/App.jsx
@@ -1,34 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("olive")
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <div className="w-full h-screen duration-200" style={{backgroundColor:color}}>
+     <div className="fixed bottom-[50%] flex justify-center inset-x-0 bg-red-600 rounded-5 flex-wrap gap-6 p-1">
+
+        
+          <div className="flex justify-center bg-white rounded-full px-1">  <button onClick={()=>setColor("black")} className="bg-black rounded-full px-4 text-white">black</button></div>
+            <div className="flex justify-center bg-white rounded-full p-1">  <button onClick={()=>setColor("grey")} className="bg-gray-400 rounded-full px-4">grey</button></div>
+            
+                <div className="flex justify-center bg-white rounded-full p-1">    <button onClick={()=>setColor("pink")} className="bg-pink-400 rounded-full px-4">pink</button></div>
+                  <div className="flex justify-center bg-white rounded-full p-1">   <button onClick={()=>setColor("green")} className="bg-green-400 rounded-full px-4">green</button></div>
+                    <div className="flex justify-center bg-white rounded-full p-1">  <button onClick={()=>setColor("blue")} className="bg-blue-400 rounded-full px-4">blue</button></div>
+                      <div className="flex justify-center bg-white rounded-full p-1">   <button onClick={()=>setColor("yellow")} className="bg-yellow-400 rounded-full px-4">yellow</button></div>
+                        <div className="flex justify-center bg-white rounded-full p-1">  <button onClick={()=>setColor("blue")} className="bg-blue-400 rounded-full px-4">blue</button></div>
+     </div>
+   </div>
   )
 }
 
--- vite/pr5/src/App.jsx
+++ vite/pr5/src/App.jsx
@@ -1,34 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState,useCallback,useEffect} from 'react'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length,setLength]=useState(8)
+  const [charAllow,setCharAllow]=useState(false)
+  const [numAllow,setNumAllow]=useState(false)
+  const [password,setPassword]=useState("")
+  const passwordGenerator=useCallback(()=>{
+    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" 
+    let pass=""
+    if(numAllow)str+="0123456789" 
+    if(charAllow)str+="@#₹&/*:;!€¥$¢√π÷×¶∆|}{"
+    for(let i=0;i<length;i++){
+      let char=Math.floor(Math.random()*str.length+1)
+      pass+=str.charAt(char)
+    }
+    
+    setPassword(pass)
+    
+  } ,[length,charAllow,numAllow,setPassword])
+  useEffect(()=>{
+    passwordGenerator()
+  },[passwordGenerator,length,numAllow,charAllow])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  <div className="w-full bg-gray-400 max-w-md mx-auto px-5 py-7 rounded-3xl">
+    <h1>Password Generator</h1>
+    <div className="flex  rounded-3xl gap-1">
+      <input type="text"
+      value={password}
+      placeholder="password"
+      className="bg-sky-500 rounded-lg"
+      readonly/>
+       <button className="rounded-lg bg-yellow-300 px-2 py-1" >copy</button>
+    </div>
+    <div className="flex py-3 gap-1 flex-wrap">
+      <input type="range"
+      min={6}
+      max={100}
+      value={length}
+      onChange={(e)=>setLength(e.target.value)}
+      />
+      <label>length:{length}</label>
+      <input
+      type="checkbox" 
+      onChange={()=>{
+        setNumAllow(prev=>!prev)
+      }}
+      />
+       <label>number</label>
+       <input
+      type="checkbox" 
+      onChange={()=>{
+        setCharAllow(prev=>!prev)
+      }}
+      />
+        <label>chareacter</label>
+    </div>
+   
+  </div>
   )
 }
 
