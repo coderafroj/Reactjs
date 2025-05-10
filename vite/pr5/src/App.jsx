f5cf50cc122b99daf36723de0d824a2504bd5c35
coderafroj <coderafroj@gmail.com>
2025-05-10 14:09:19 +0530

    update

--- vite/pr5/src/App.jsx
+++ vite/pr5/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState,useCallback,useEffect} from 'react'
+import { useState,useCallback,useEffect,useRef} from 'react'
 
 
 function App() {
@@ -12,27 +12,35 @@ function App() {
     if(numAllow)str+="0123456789" 
     if(charAllow)str+="@#₹&/*:;!€¥$¢√π÷×¶∆|}{"
     for(let i=0;i<length;i++){
-      let char=Math.floor(Math.random()*str.length+1)
+      let char=Math.floor(Math.random()*str.length)
       pass+=str.charAt(char)
     }
     
     setPassword(pass)
     
   } ,[length,charAllow,numAllow,setPassword])
+  const pr=useRef(null)
+  const copyClipBoard=useCallback(()=>{
+    pr.current.select()
+   
+    window.navigator.clipboard.writeText(password)
+  })
   useEffect(()=>{
     passwordGenerator()
   },[passwordGenerator,length,numAllow,charAllow])
 
   return (
   <div className="w-full bg-gray-400 max-w-md mx-auto px-5 py-7 rounded-3xl">
-    <h1>Password Generator</h1>
-    <div className="flex  rounded-3xl gap-1">
+    <h1 className="text-center">Password Generator</h1>
+    <div className="flex justify-around rounded-3xl gap-.5 bg-sky-300">
       <input type="text"
       value={password}
       placeholder="password"
-      className="bg-sky-500 rounded-lg"
-      readonly/>
-       <button className="rounded-lg bg-yellow-300 px-2 py-1" >copy</button>
+      className="bg-white rounded-lg"
+      readOnly
+      ref={pr}
+      />
+       <button onClick={copyClipBoard} className="rounded-lg bg-yellow-300 px-2 py-1 focus:bg-blue-400" >copy</button>
     </div>
     <div className="flex py-3 gap-1 flex-wrap">
       <input type="range"
@@ -41,21 +49,21 @@ function App() {
       value={length}
       onChange={(e)=>setLength(e.target.value)}
       />
-      <label>length:{length}</label>
+      <label className="text-sm">length:{length}</label>
       <input
       type="checkbox" 
       onChange={()=>{
         setNumAllow(prev=>!prev)
       }}
       />
-       <label>number</label>
+       <label className="text-sm">number</label>
        <input
       type="checkbox" 
       onChange={()=>{
         setCharAllow(prev=>!prev)
       }}
       />
-        <label>chareacter</label>
+        <label className="text-sm">char</label>
     </div>
    
   </div>
