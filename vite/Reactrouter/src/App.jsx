7ac0d195695f7c4566705f9dec96d8e98ce89765
coderafroj <coderafroj@gmail.com>
2025-06-11 15:36:08 +0530

    add

---
+++ vite/Reactrouter/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+   <h1 className="text-6xl text-red-500">React router</h1>
+  )
+}
+
+export default App
