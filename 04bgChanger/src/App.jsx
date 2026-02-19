a440cf3372a5e24de279845cd507780f30c68946
bytecore <bytecore.info@gmail.com>
2026-02-19 16:24:15 +0530

    add

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 
 
 function App() {
- const [color,setColor]=useState('navy')
+ const [color,setColor]=useState('olive')
 
   return (
     <div className="w-full h-screen" style={{backgroundColor:color}}>
