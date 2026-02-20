e5735fd52056b287480ccfae5ee52c95eff24e2c
bytecore <bytecore.info@gmail.com>
2026-02-20 12:24:27 +0530

    add

---
+++ 05passwordGen/src/App.jsx
@@ -0,0 +1,73 @@
+import { useCallback, useEffect, useRef, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [length,setLenght]=useState(8)
+  const [num,setNum]=useState(false)
+  const [char,setChar]=useState(false)
+  const [password,setPassword]=useState("")
+ 
+
+  const passwordGen=useCallback(()=>{
+    let pass=""
+     let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+     if(num)str+="0123456789"
+     if(char)str+="[]{}'&*#$%^&*()"
+     for(let i=1;i<=length;i++){
+      let character=Math.floor(Math.random()*str.length+1)
+      pass+=str.charAt(character)
+     }
+     setPassword(pass)
+
+  },[length,char,num,setPassword])
+  useEffect(()=>{
+    passwordGen()
+  },[length,char,num])
+
+  //useref==password referace and use ref atriubte
+  const passRef=useRef(null)
+// const copyBtn=()=>{
+//   window.navigator.clipboard.writeText(password)
+// }
+// optimize
+const copyBtn=useCallback(()=>{
+  
+  window.navigator.clipboard.writeText(password)
+  passRef.current.select(password)//copy krn epr high lifht ho jayega
+  passRef.current.setSelectionRange(0,3)//isse apne hisaas se letter copy kr skte hai  jitne krna ho
+
+},[password])
+  return (
+    <div className='w-full flex justify-center rounded-3xl py-6 px-3 shadow-2xs items-center'>
+      
+      <div className='max-w-md bg-red-400 py-6 px-6 rounded-4xl'>
+        <h2 className='capitalize mb-3'>password generator</h2>
+        <div className='mx-w-md w-full'>
+          <input type="text" className='bg-white rounded-l-lg px-2 py-1 w-[80%] outline-none 'ref={passRef} readOnly value={password} />
+          <button onClick={copyBtn} className='rounded-r-lg bg-blue-800 px-2 py-1 cursor-pointer'>copy</button>
+        </div>
+        <div className='flex justify-center items-center gap-9 mt-2'>
+          <div><input type="range" className='mr-2'
+          value={length}
+          min={6}
+          max={100}
+          onChange={(e)=>setLenght(e.target.value)}
+          />
+          <label htmlFor="">len:{length}</label>
+          </div>
+          <div><input type="checkbox" onChange={()=>setChar(prev=>!prev)} />
+          <label htmlFor="">char</label>
+          </div>
+          <div><input type="checkbox" onChange={()=>setNum(prev=>!prev)}  />
+          <label htmlFor="">num</label>
+          </div>
+        </div>
+      </div>
+
+    </div> 
+  )
+}
+
+export default App
--- 3rdProps/src/App.jsx
+++ 3rdProps/src/App.jsx
@@ -9,15 +9,12 @@ function App() {
   return (
     <>
       <h1 className='text-orange-600'>coderafroj </h1>
-<<<<<<< HEAD
       <Card name="sohil beta " data={s} b={{name:'afroj'}} job="barber" logo={logo}/>
       <Card name="rahul babu" job='founder of bytecore' logo={logo}/>
       <Card name="coderafroj" job='berozgaar' logo={logo}/>
-=======
       <Card name="sohil beta " job="barber"/>
         <Card name="ram lakhan " job="cs teacher"/>
 
->>>>>>> ced4eb26594b73d1774cf625a5d06b929f3734a7
     </>
   )
 }
