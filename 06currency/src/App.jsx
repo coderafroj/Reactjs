bd8ad19f02a389d13b5de35bfe33fea9d584d165
bytecore <bytecore.info@gmail.com>
2026-02-25 10:37:48 +0530

    add

--- 06currency/src/App.jsx
+++ 06currency/src/App.jsx
@@ -1,28 +1,60 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Input from './components/Input'
+import Input from './components'
+import useCurrecyInfo from './hooks/useCurrencyInfo'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount,setAmount]=useState(0)
+  const [to,setTo]=useState('inr')
+  const [from,setFrom]=useState('usd')
+  const [convertedAmount,setConvertedAmount]=useState()
+ 
+  const currencyInfo=useCurrecyInfo(from)
+  const options=Object.keys(currencyInfo)
+  const swap=()=>{
+    setFrom(to)
+    setTo(from)
+
+    setConvertedAmount(amount)
+    setAmount(convertedAmount)
+  }
+   const convert=()=>{
+     setConvertedAmount(amount*currencyInfo[to])
+   }
+
+
 
   return (
 <div className='w-full flex flex-wrap justify-center h-screen bg-no-repeat bg-cover items-center' style={{backgroundImage:`url(https://plus.unsplash.com/premium_photo-1682310075673-b408eb1ca6fd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dHJhZGluZ3xlbnwwfHwwfHx8MA%3D%3D)`}}>
   <div className='w-full'>
     <div className='w-full max-w-md mx-auto backdrop-blur-sm rounded-lg  border border-gray-100
      bg-white/40'>
-      <form action="">
+      <form 
+      onSubmit={(e)=>{
+        e.preventDefault()
+        convert()
+      }}
+      >
         <div className='w-full mb-4'>
-          <Input label='from'/>
+          <Input label='from' 
+          currencyOption={options} 
+          amount={amount} 
+          onAmountChange={(amount)=>setAmount(amount)}
+          selectCurrecy={from}
+          onCurrencyChange={(currency)=>setAmount(currency)}
+          />
         </div>
         <div className='relative w-full'>
-          <button className='absolute left-1/2 -translate-x-1/2 -translate-y-1/2 bg-blue-200 rounded-lg px-2 py-0.5'>swap</button>
+          <button className='absolute left-1/2 -translate-x-1/2 -translate-y-1/2 bg-blue-200 rounded-lg px-2 py-0.5' onClick={swap}>swap</button>
         </div>
         <div className='w-full'>
-          <Input label='to'/>
+          <Input label='to'  
+          amount={convertedAmount} 
+          currencyOption={options} 
+          onCurrencyChange={(currency)=>setTo(currency)} 
+          selectCurrecy={to}/>
         </div>
-        <button className='bg-blue-600 rounded-lg px-2 py-0.5 mb-3 mt-3 '>COnvert</button>
+        <button type="submit" className='bg-blue-600 rounded-lg px-2 py-0.5 mb-3 mt-3 ' onClick={convert}>Convert {from.toUpperCase()}to {to.toLowerCase()}</button>
       </form>
     </div>
   </div>
--- 06currency/src/components/Input.jsx
+++ 06currency/src/components/Input.jsx
@@ -1,29 +1,46 @@
+import { useId } from "react"
+
 function Input({
-    label
+    label,
+    amount,
+    onAmountChange,
+    onCurrencyChange,
+    selectCurrecy,
+    currencyOption=[]
 }){
+    const amountInputId=useId()
     return(
-<div className="flex">
+<div className="flex px-3 py-1">
    <div className="w-1/2">
-                <label  className="text-black/40 mb-2 inline-block">
+                <label htmlFor={amountInputId}  className="text-black/40 mb-2 inline-block">
                     {label}
                 </label>
                 <input
-                    
+                    id={amountInputId}
                     className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount"
+                    value={amount}
+                    onChange={(e)=>onAmountChange && onAmountChange(Number(e.target.value))}
+                    
                 />
             </div>
     <div className="w-1/2 flex flex-wrap justify-end text-right">
                 <p className="text-black/40 mb-2 w-full">Currency Type</p>
                 <select
                     className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+                    value={selectCurrecy}
+                    onChange={(e)=>onCurrencyChange && onCurrencyChange(e.target.value)}
                     
                 >
                     
-                        <option value="usd">
-                            usd
-                        </option>
+                        {
+                            currencyOption.map((c)=>(
+                                <option value={c} key={c}>
+                                    {c}
+                                    </option>
+                            ))
+                        }
                 
                 </select>
             </div>
