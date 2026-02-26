2e56a9e46ed492441061a2e53fc6d166870dd819
bytecore <bytecore.info@gmail.com>
2026-02-26 13:33:34 +0530

    add

--- 07router/src/components/About/About.jsx
+++ 07router/src/components/About/About.jsx
@@ -2,12 +2,12 @@ import avtar from '../../assets/avtr.jpg'
 export default function About(){
     return(
         <div className="w-full bg-black h-screen text-white">
-                    <div className='max-w-md w-full h-screen flex justify-center items-center mx-auto gap-32 '>
+                    <div className='max-w-md w-full h-screen md:flex-row  flex flex-col justify-center items-center mx-auto md:gap-32 gap-6 '>
                         <div className=' '>
             <div className='w-full  '><img className='rounded-2xl mt-0' src={avtar} alt="" /></div>
                         </div>
-                        <div className='w-1/2'>
-                            <h2>myself ByteCore </h2>
+                        <div className='w-1/2 text-center'>
+                            <h2 className='text-2xl capitalize  '>myself ByteCore </h2>
                         </div>
                     </div>
                 </div>
--- 07router/src/components/Home/Home.jsx
+++ 07router/src/components/Home/Home.jsx
@@ -1,12 +1,12 @@
 import avtar from '../../assets/avtr.jpg'
 export default function Home(){
     return(
-        <div className="w-full bg-black h-screen text-white">
-            <div className='max-w-md w-full h-screen md:flex-row flex flex-col justify-center items-center mx-auto md:gap-32  gap-5'>
-                <div className=' '>
-    <div className='w-full  '><img className='rounded-2xl mt-0' src={avtar} alt="" /></div>
+        <div className="w-full bg-black flex justify-center min-h-screen items-center text-white px-6">
+            <div className=' w-full md:flex-row flex flex-col justify-center items-center mx-auto max-w-6xl'>
+                <div className='w-full md:w-1/2 flex justify-center'>
+    <div className=' w-60'><img  className='rounded-2xl mt-0 w-60 h-60 object-cover  ' src={avtar} alt="" /></div>
                 </div>
-                <div className='w-1/2 text-center'>
+                <div className='w-full md:w-1/2 text-center md:text-left'>
                     <h2 className='text-3xl capitalize'>myself ByteCore </h2>
                 </div>
             </div>
