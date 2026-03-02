d82e1250c932a37a86281b251c81e4753a9e306b
bytecore <bytecore.info@gmail.com>
2026-03-02 10:27:43 +0530

    add

--- 07router/src/main.jsx
+++ 07router/src/main.jsx
@@ -8,31 +8,44 @@ import About from './components/About/About'
 import Github,{githubInfo} from './components/Github/Github'
 import Params from "./components/Params/Params"
 
-const router=createBrowserRouter([
-  {
-    path:"/",
-    element:<App/>,
-    children:[
-      {
-        path:"",
-        element:<Home/>
-      },
-      {
-        path:"about",
-        element:<About/>
-      },
-      {
-        path:'github',
-        element:<Github/>,
-        loader:githubInfo
-      },
-      {
-      	path:'Params/:id',
-      	element:<Params/>
-      }
-    ]
-  }
-])
+// const router=createBrowserRouter([
+//   {
+//     path:"/",
+//     element:<App/>,
+//     children:[
+//       {
+//         path:"",
+//         element:<Home/>
+//       },
+//       {
+//         path:"about",
+//         element:<About/>
+//       },
+//       {
+//         path:'github',
+//         element:<Github/>,
+//         loader:githubInfo
+//       },
+//       {
+//       	path:'Params/:id',
+//       	element:<Params/>
+//       }
+//     ]
+//   }
+// ])
+const router=createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App/>}>
+      <Route path="" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path='/github' element={<Github/>} loader={githubInfo}/>
+      <Route path='/params/:id' element={<Params/>}/>
+      
+
+
+    </Route>
+  )
+)
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router}/>
