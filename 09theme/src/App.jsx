5688873ca91ffa409c61499753bd475d25d1a761
bytecore <bytecore.info@gmail.com>
2026-03-09 18:08:23 +0530

    add

--- 09theme/src/App.jsx
+++ 09theme/src/App.jsx
@@ -1,14 +1,41 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
+import { ThemeProvider } from './contexts/Theme'
 import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+const [themeMode,setThemeMode]=useState("light")
+const lightTheme=()=>{
+  setThemeMode('light')
+}
+const darkTheme=()=>{
+  setThemeMode('dark')
+}
+useEffect(()=>{
+  document.querySelector("html").classList.remove('dark','light')
+document.querySelector('html').classList.add(themeMode)
+},[themeMode])
 
   return (
-    <ThemeBtn/>
+    <ThemeProvider value={{themeMode,lightTheme,darkTheme}}>
+   
+<div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        <ThemeBtn/>
+                    </div>
+
+                    <div className="w-full max-w-sm mx-auto">
+                       <Card/>
+                    </div>
+                </div>
+            </div>
+            </ThemeProvider>
+
   )
 }
 
